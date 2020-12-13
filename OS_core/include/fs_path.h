579ca75e89b6e0a48da1de45cb1543e5de5257b1
KaguyaShinsya <az1838614890@vip.qq.com>
2020-12-13 22:52:31 +0800

    imgfs some bugs fixed

--- OS_core/include/fs_path.h
+++ OS_core/include/fs_path.h
@@ -11,14 +11,61 @@
 #include <calls.h>
 #include <types.h>
 
+/** format path string. like "/var\..///a/./c" to "/var/../a/c" **/
+int fs_path_format(char* path, char** path_buffer){
+    if(!path_buffer || !path)
+        return -1;
+    char buff[FS_PATH_MAX] = {0};
+    int path_len = strlen(path);
+    int buff_len = path_len;
+    memcpy(buff, path, path_len);
+    bool undone;
+    do{
+        undone = false;
+        for(int i = 0; i < buff_len; i++){
+            if(buff[i] == '\\'){
+                buff[i] = '/';
+                undone = true;
+            }else if(buff[i] == '/'){
+                if(i>0){
+                    if(buff[i-1] == '/'){
+                        memcpy(&buff[i-1], &buff[i], buff_len - i);
+                        buff_len--;
+                        undone = true;
+                        break;
+                    }else if(i < buff_len-1 && i > 1){
+                        if(buff[i-1] == '.' && buff[i-2] == '/'){
+                            undone = true;
+                            memcpy(&buff[i-2], &buff[i], buff_len - i);
+                            buff_len-=2;
+                        }
+                    }
+                }
+            }
+        }
+    }while (undone);
+    char* ret = (char*)malloc(buff_len+1);
+    ret[buff_len] = '\0';
+    memcpy(ret, buff, buff_len);
+    *path_buffer = ret;
+    return buff_len;
+}
+
 /** get max level of path **/
 path_level fs_get_path_level(char* path_buffer){
+    char* format_t = 0;         /** format string **/
+    fs_path_format(path_buffer, &format_t);
+    if(!format_t)
+        return -1;
+    path_buffer = format_t;
+
     int len = strlen(path_buffer);
     if(len < 2 || !path_buffer)
         return -1;
     int subs = 0, level = -1;
     if((path_buffer[0] != '.') && (path_buffer[0] != '/'))
         return -1;
+
     for(;subs<len;subs++){
         if(path_buffer[subs] == '/') {
             if (subs > 0){
@@ -38,6 +85,7 @@ path_level fs_get_path_level(char* path_buffer){
             }
         }
     }
+    free(format_t);
     if(path_buffer[len-1] == '/')
         level--;
     return level;
@@ -45,6 +93,12 @@ path_level fs_get_path_level(char* path_buffer){
 
 /** get filename of path by one level **/
 char* fs_get_path_name(char* path_buffer, path_level target_level){
+    char* format_t = 0;         /** format string **/
+    fs_path_format(path_buffer, &format_t);
+    if(!format_t)
+        return 0;
+    path_buffer = format_t;
+
     if(fs_get_path_level(path_buffer) == -1)
         return 0;
     char buff[FS_FILENAME_MAX] = {0};
@@ -85,6 +139,7 @@ char* fs_get_path_name(char* path_buffer, path_level target_level){
             buff_len++;
         }
     }
+    free(format_t);
     if(buff_len == 0)
         return 0;
     char* ret = (char*)malloc(buff_len+1);
@@ -97,6 +152,20 @@ char* fs_get_path_name(char* path_buffer, path_level target_level){
 int fs_get_path_merge(char* work_path, char* path_buffer, char** merge_buffer){
     if(!work_path || !path_buffer || !merge_buffer)
         return -1;
+
+    char* format_t = 0;         /** format string **/
+    fs_path_format(path_buffer, &format_t);
+    if(!format_t)
+        return -1;
+    path_buffer = format_t;
+    format_t = 0;
+    fs_path_format(work_path, &format_t);
+    if(!format_t){
+        free(path_buffer);
+        return -1;
+    }
+    work_path = format_t;
+
     int work_path_len = strlen(work_path);
     int path_len = strlen(path_buffer), p_start = 0;
     int merge_len = 0, loop_t = 0;
@@ -127,6 +196,8 @@ int fs_get_path_merge(char* work_path, char* path_buffer, char** merge_buffer){
                         undone = true;
                         if(last_split == -1){   /** like "/../xxx" is illegal **/
                             *merge_buffer = 0;
+                            free(work_path);
+                            free(path_buffer);
                             return -1;
                         }
                         memcpy(&merge_tmp[last_split], &merge_tmp[loop_t+3], merge_len - (loop_t+3));
@@ -139,6 +210,8 @@ int fs_get_path_merge(char* work_path, char* path_buffer, char** merge_buffer){
             }
         }
     }while (undone);
+    free(work_path);
+    free(path_buffer);
     char* ret = (char*)malloc(merge_len+1);
     memset(ret,0,merge_len+1);
     memcpy(ret, merge_tmp, merge_len);
@@ -150,6 +223,20 @@ int fs_get_path_merge(char* work_path, char* path_buffer, char** merge_buffer){
 int make_path(char** full_path_buffer, char* dir, char* filename){
     if(!filename || !full_path_buffer || !dir)
         return -1;
+
+    char* format_t = 0;         /** format string **/
+    fs_path_format(dir, &format_t);
+    if(!format_t)
+        return -1;
+    dir = format_t;
+    format_t = 0;
+    fs_path_format(filename, &format_t);
+    if(!format_t) {
+        free(dir);
+        return -1;
+    }
+    filename = format_t;
+
     char tmp[FS_PATH_MAX+1] = {0};
     int fl = strlen(filename), dl = strlen(dir);
     if(fl + dl > FS_PATH_MAX)
@@ -173,6 +260,32 @@ int make_path(char** full_path_buffer, char* dir, char* filename){
     memcpy(ret, tmp, len);
     *full_path_buffer = ret;
     return len;
+}
+
+int fs_path_check_illegal(char* path){
+    if(!path)
+        return -1;
+    int len = strlen(path);
+    for(int i = 0; i < len; i++){
+        if(((path[i] > 'a' && path[i] < 'z') || (path[i] > 'A' && path[i] < 'Z') || (path[i] > '0' && path[i] < '9')));
+        else if((path[i] == '.' || path[i] == '-' || path[i] == '+' || path[i] == '=' || path[i] == '_'));
+        else return 1;
+    }
+    return 0;
+}
+
+int fs_string_cmp(char* a, char* b){
+    if(!a || !b)
+        return -1;
+    int al = strlen(a);
+    int bl = strlen(b);
+    if(al != bl)
+        return 1;
+    for(int i = 0; i < al; i++){
+        if(a[i] != b[i])
+            return 1;
+    }
+    return 0;
 }
 
 #endif //OS_VFS_DEBUG_FS_PATH_H
--- OS_core/vfs/imgfs/imgfs_struct.h
+++ OS_core/vfs/imgfs/imgfs_struct.h
@@ -4,14 +4,18 @@
 #define OS_VFS_DEBUG_IMGFS_STRUCT_H
 
 #include <stdlib.h>
+#include <new>
 #include <types.h>
 #include <calls.h>
 
 #include <logservice.h>
 #include <logtime.h>
 #include <fs_struct.h>
+#include <fs_path.h>
 
 #define _IMGFS_BLOCK_SIZE 512       //bytes
+#define _IMGFS_A_FILE_P imgfs_struct::imgfs_already_file_list*
+#define _IMGFS_A_FILE imgfs_struct::imgfs_already_file_list
 
 namespace imgfs_struct{
     struct imgfs_info{  /** read only **/
@@ -27,12 +31,13 @@ namespace imgfs_struct{
     };
 
     struct imgfs_already_file_list{
-        char type;
+        char type;  // 0 - dir, 1 - file
         FILE *local;
         uint32_t pre_id;
         uint32_t parent_id;
         char* filename;
-        struct imgfs_uni_table* child_list;
+        struct _core_log_date* ctime;
+        struct imgfs_already_file_list* child_list;
         struct imgfs_already_file_list* next;
     };
 
@@ -54,97 +59,386 @@ namespace imgfs_struct{
         uint32_t cur;
     };
 
+    struct imgfs_local_struct{
+        uint32_t id;
+        uint32_t p_id;
+        char type;
+        struct _core_log_date* ctime;
+        struct imgfs_uni_table* child_list;
+    };
 }
 
 class imgfs{
 public:
-    void set_create_file_table(struct imgfs_struct::imgfs_uni_table* first_addr);
-    int create(char* local_path, uint32_t size);
+    int create(char* local_path);
     int mount(char* local_path, uint8_t mode);    /** return inode id of root in this fs **/
     int unmount();
-    int format();
-    uint32_t getFilesReset();   /** reset traversal counter **/
-    uint32_t getFilesOfParent(uint32_t p_inode);    /** get files or directories of parent directory by traversal way **/
-    _core_fs_file_struct getFileInfo(uint32_t p_inode);
-    uint32_t openFile(uint32_t p_inode);
-    char* getFileName(uint32_t p_inode);
-    uint32_t fileWrite(uint32_t p_inode, void* buffer, uint32_t size, uint32_t count);
-    uint32_t fileRead(uint32_t p_inode, void* buffer, uint32_t buffer_size, uint32_t read_size);
-    uint32_t fileSeek(uint32_t p_inode, long offset, int start_point);
-    void closeFile(uint32_t p_handle);
+    //int format();
+
+    void getFilesReset();   /** reset traversal counter **/
+    //imgfs_struct::imgfs_uni_table* getFilesOfParent(uint32_t p_inode);    /** get files or directories of parent directory by traversal way **/
+    _core_fs_file_struct* getFileInfo(uint32_t p_inode);
+    //uint32_t openFile(uint32_t p_inode);
+    //char* getFileName(uint32_t p_inode);
+    //uint32_t fileWrite(uint32_t p_inode, void* buffer, uint32_t size, uint32_t count);
+    //uint32_t fileRead(uint32_t p_inode, void* buffer, uint32_t buffer_size, uint32_t read_size);
+    //uint32_t fileSeek(uint32_t p_inode, long offset, int start_point);
+    //void closeFile(uint32_t p_handle);
     uint32_t getFileCount();
     _core_log_date* getCreateTime();
     _core_log_date* getChangeTime();
+    double getCreateProcess();
 
+    //API:
+    int create_directory_table_insert(char* path);
+    int create_file_table_insert(char* local_path, char* fs_path);
+    char* SearchFile(char* filename);
+    uint32_t OpenFile(char* filename);
+    void CloseFile(uint32_t handle);
+    uint32_t FileSeek(uint32_t handle, uint32_t offset, uint32_t start_p);
+    uint32_t FileRead(uint32_t handle, void* buffer, uint32_t read_len);
+    imgfs_struct::imgfs_uni_table* getFilesOfParent(char* p_dir);
 
+    imgfs();
+    ~imgfs();
 protected:
     struct imgfs_struct::imgfs_info info;
+    double createProcess;
 private:
-    struct imgfs_struct::imgfs_uni_table* file_list_t;
-    FILE *local;
-    //void info_sync();
+    struct _IMGFS_A_FILE_P already_file_list;   /** root **/
+    FILE *local;    /** local img file **/
+    uint32_t already_ino;   /** next id **/
+
+    uint32_t getFileOfParent(uint32_t p_inode, char* filename);
+
+    void* already_file_get(_IMGFS_A_FILE_P p_struct, char* name);
+    void* already_dir_append(_IMGFS_A_FILE_P p_struct, char* name);
+    void* already_file_append(_IMGFS_A_FILE_P p_struct, char* name, char* local_path);
+    void already_list_clear(_IMGFS_A_FILE_P p);
+
 };
 
-void imgfs::set_create_file_table(struct imgfs_struct::imgfs_uni_table *first_addr) {
-    if(!first_addr)
-        return;
-    file_list_t = first_addr;
+imgfs::imgfs() {
+    already_file_list = new _IMGFS_A_FILE();
+    printf("root: %p\n", already_file_list);
+    createProcess = 0.0;
+    already_ino = 1;
+    info.inode_count = 0;
 }
 
-int imgfs_pre_dir_level(char *path){
-    int len = strlen(path);
-    printf("%d  ",len);
-    if(len < 2)return -1;
-    int ret = 0;
-    bool first = false, back = false;
-    for(int i = 0; i < len; i++){
-        if(path[i] == '/'){
-            if(first) {
-                if(!back){
-                    if (i > 0) {
-                        if (path[i - 1] != '/')
-                            ret++;
-                    }else if (!i)
-                        ret++;
-                }else{
-                    back = false;
-                }
-            }else{
-                first = true;
-            }
-        }else if(path[i] == '.'){
-            if(i > 0){
-                if(path[i-1] == '.'){   /** back to parent dir like '/root/../var' it's level is 1 **/
-                    if(i < len - 1){
-                        if(path[i+1] == '/'){
-                            ret--;
-                            back = true;
-                        }
-                    }
-                }
-            }
+void imgfs::already_list_clear(_IMGFS_A_FILE_P p){
+    if(p->next){
+        already_list_clear(p->next);
+    }
+    if(p->child_list){
+        already_list_clear(p->child_list);
+    }
+
+    if(p->ctime)free(p->ctime);
+    if(p->filename)delete [] (char*)p->filename;
+    if(p->local)fclose(p->local);
+    delete p;
+}
+
+imgfs::~imgfs() {
+    _core_service_log_print("[imgfs] cleaning.");
+    already_list_clear(already_file_list);
+    if(local)
+        fclose(local);
+}
+
+void* imgfs::already_file_get(_IMGFS_A_FILE_P p_struct, char* name){
+    if(!p_struct || !name)
+        return 0;
+    _IMGFS_A_FILE_P op_c = p_struct->child_list;
+    while(op_c){
+        //printf("[debug string cmp] |=%s=|=%s=|\n", op_c->filename, name);
+        if(fs_string_cmp(op_c->filename, name) == 0){
+            return op_c;
+        }
+        op_c = op_c->next;
+    }
+    return nullptr;
+}
+
+void* imgfs::already_dir_append(_IMGFS_A_FILE_P p_struct, char* name){
+    _IMGFS_A_FILE_P tmp = (_IMGFS_A_FILE_P)already_file_get(p_struct, name);
+    _IMGFS_A_FILE_P table_t;
+    if(tmp)
+        return 0;  /** already exists **/
+    if(!p_struct || !name)
+        return 0;
+    if(p_struct->type)
+        return 0;  /** parent is not a dir **/
+
+    printf("add to parent - %p\n", p_struct);
+
+    table_t = p_struct->child_list;
+    if(table_t){
+        while(table_t->next)
+            table_t = table_t->next;
+        table_t->next = new _IMGFS_A_FILE();
+        table_t=table_t->next;
+    }else{
+        p_struct->child_list = new _IMGFS_A_FILE();
+        table_t = p_struct->child_list;
+    }
+    table_t->parent_id = p_struct->pre_id;
+    table_t->filename = new char[FS_FILENAME_MAX+1]();
+    memcpy(table_t->filename, name, strlen(name));
+    table_t->filename[strlen(name)] = '\0';
+    table_t->pre_id = already_ino;
+    already_ino++;
+    table_t->ctime = _core_log_date_get();
+    _core_service_log_print("[imgfs] new directory named:");
+    _core_service_log_print(name);
+    return table_t;
+}
+
+void* imgfs::already_file_append(_IMGFS_A_FILE_P p_struct, char *name, char *local_path) {
+    _IMGFS_A_FILE_P tmp = (_IMGFS_A_FILE_P)already_file_get(p_struct, name);
+    _IMGFS_A_FILE_P table_t;
+    if(tmp || !p_struct || !name || !local_path || p_struct->type)
+        return 0;
+
+    _core_service_log_print("[imgfs] try to create file:");
+    _core_service_log_print(name);
+    _core_service_log_print("[imgfs] new file from");
+    _core_service_log_print(local_path);
+
+    FILE* local_t = fopen(local_path, "r");
+    if(!local_t) {
+        _core_service_log_print("[imgfs] append file failed. can't open file.");
+        return 0;
+    }
+
+    _core_service_log_print("processing.");
+
+    table_t=p_struct->child_list;
+    /**debug**/
+    //printf("p_ptr:%p parent_id: %d\ntable_t: %p\n", p_struct, p_struct->parent_id, table_t);
+
+    if(table_t){
+        while(table_t->next){
+            table_t=table_t->next;
+            putchar('-');
+        }
+        try{
+            table_t->next = new _IMGFS_A_FILE();
+        } catch (std::bad_alloc) {
+            _core_service_log_print("[imgfs] bad alloc.");
+            _core_service_log_print("[imgfs] processing failed.");
+            return 0;
+        }
+        printf("child ptr: %p\n", table_t->next);
+        table_t = table_t->next;
+    }else{
+        try{
+            p_struct->child_list = new _IMGFS_A_FILE();
+        }catch (std::bad_alloc) {
+            _core_service_log_print("[imgfs] bad alloc.");
+            _core_service_log_print("[imgfs] processing failed.");
+            return 0;
+        }
+        printf("child ptr: %p\n", p_struct->child_list);
+        table_t=p_struct->child_list;
+    }
+
+    _core_service_log_print("processing done. making.");
+
+    table_t->parent_id = p_struct->pre_id;
+    table_t->filename = new char[FS_FILENAME_MAX+1]();
+    memcpy(table_t->filename, name, strlen(name));
+    table_t->filename[strlen(name)] = '\0';
+    /** debug **/
+    //printf("%d |%s|%s|\n", strlen(name), table_t->filename, name);
+    table_t->local = local_t;
+    table_t->type = 1;
+    table_t->pre_id = already_ino;
+    already_ino++;
+    table_t->ctime = _core_log_date_get();
+
+    _core_service_log_print("making done.");
+    return table_t;
+}
+
+int imgfs::create_file_table_insert(char* local_path, char* fs_path) {
+    if(!local_path || !fs_path)
+        return -1;
+    if(fs_path_check_illegal(fs_path))
+        return -5;
+    _IMGFS_A_FILE_P p_struct = already_file_list;
+    char* path_t = 0;
+    int path_len = fs_path_format(fs_path, &path_t);
+    if(!path_t || path_len < 2)
+        return -1;
+    path_level lvl_t = fs_get_path_level(path_t);
+    char* name_t;
+    for(int i = 0; i < lvl_t; i++){
+        if(p_struct){
+            name_t = fs_get_path_name(path_t, i);
+            /** debug **/
+            //printf("lvl %d of %s is %s\n", i, path_t, name_t);
+            p_struct = (_IMGFS_A_FILE_P)already_file_get(p_struct, name_t);
+            if(name_t)free(name_t);
+
         }
     }
-    if(path[len-1] == '/')
-        ret--;
-    if(path[len-1] == '.')  /** it's illegal like '/var/..' or '/var/.' **/
-        return -1;
-    return ret;
+    if(!p_struct){
+        _core_service_log_print("[imgfs] can't append file, the parent directory is not exist.");
+        free(path_t);
+        return -2;
+    }
+    name_t = fs_get_path_name(path_t, lvl_t);
+    _IMGFS_A_FILE_P this_file = (_IMGFS_A_FILE_P)already_file_get(p_struct, name_t);
+    if(this_file){
+        _core_service_log_print("[imgfs] can't append file, it was already exist.");
+        if(name_t)
+            free(name_t);
+        free(path_t);
+        return -3;
+    }
+    this_file = (_IMGFS_A_FILE_P)already_file_append(p_struct, name_t, local_path);
+    if(!this_file){
+        _core_service_log_print("[imgfs] can't append file, not reason.");
+        if(name_t)
+            free(name_t);
+        free(path_t);
+        return -4;
+    }
+    _core_service_log_print("[imgfs] touch file:");
+    _core_service_log_print(path_t);
+    if(name_t)
+        free(name_t);
+    free(path_t);
+    return this_file->pre_id;
 }
 
-int imgfs_pre_get_filename(char *path, uint32_t level, char *buffer){
-    int _lvl = imgfs_pre_dir_level(path);
-    if(level > _lvl)
+int imgfs::create_directory_table_insert(char* path){
+    _IMGFS_A_FILE_P parent_t = already_file_list;
+    if(fs_path_check_illegal(path))
+        return -5;
+    if(!path)
         return -1;
-    int len = strlen(path);
-    char name_t[FILENAME_MAX] = {0};
-    uint32_t now_lvl = 0;
-
-
-    return strlen(name_t);
+    char* path_t = 0;
+    int path_len = fs_path_format(path, &path_t);
+    if(path_len < 2 || !path_t)
+        return -1;
+    path_level lvl_t = fs_get_path_level(path_t);
+    char* name_t;
+    for(int i = 0; i < lvl_t; i++){
+        name_t = fs_get_path_name(path_t, i);
+        parent_t = (_IMGFS_A_FILE_P)already_file_get(parent_t, name_t);
+        if(name_t)
+            free(name_t);
+    }
+    if(!parent_t){
+        _core_service_log_print("[imgfs] can't append directory, the parent was already exist.");
+        free(path_t);
+        return -2;
+    }
+    if(parent_t->type){
+        _core_service_log_print("[imgfs] can't append directory, the parent dir not exist.");
+        free(path_t);
+        return -3;
+    }
+    name_t = fs_get_path_name(path_t, lvl_t);
+    _IMGFS_A_FILE_P this_file = (_IMGFS_A_FILE_P)already_file_get(parent_t, name_t);
+    if(this_file){
+        _core_service_log_print("[imgfs] can't append directory, it was already exist.");
+        if(name_t)
+            free(name_t);
+        free(path_t);
+        return -3;
+    }
+    this_file = (_IMGFS_A_FILE_P)already_dir_append(parent_t, name_t);
+    if(!this_file){
+        _core_service_log_print("[imgfs] can't append directory.");
+        if(name_t)
+            free(name_t);
+        free(path_t);
+        return -3;
+    }
+    _core_service_log_print("[imgfs] mkdir:");
+    _core_service_log_print(path_t);
+    if(name_t)
+        free(name_t);
+    free(path_t);
+    return this_file->pre_id;
 }
 
-int imgfs::create(char *local_path, unsigned int size) {
+void cleanUniTable(imgfs_struct::imgfs_uni_table* first_p){
+    if(first_p->next)
+        cleanUniTable(first_p->next);
+    if(first_p->data)
+        delete [] (char*)(first_p->data);
+    delete first_p;
+}
+
+imgfs_struct::imgfs_uni_table* makeUniTable(_IMGFS_A_FILE_P dir_p){
+    imgfs_struct::imgfs_uni_table* first = new imgfs_struct::imgfs_uni_table();
+    imgfs_struct::imgfs_uni_table* op = first;
+
+    //printf("making Uni table of %p\n", dir_p);
+    dir_p = dir_p->child_list;
+    bool undo = true;
+    while(dir_p){
+        if(dir_p->filename){
+            undo = false;
+            op->data = new char[FS_FILENAME_MAX]();
+            memcpy(op->data, dir_p->filename, FS_FILENAME_MAX);
+            op->next = new imgfs_struct::imgfs_uni_table();
+            op = op->next;
+        }
+        dir_p = dir_p->next;
+    }
+    if(undo){
+        if(first->data)delete[] (char*)first->data;
+        delete first;
+        return 0;
+    }
+    return first;
+}
+
+imgfs_struct::imgfs_uni_table* imgfs::getFilesOfParent(char* p_dir){
+    if(!p_dir)
+        return 0;
+    char* format_path = 0;
+    int len = fs_path_format(p_dir, &format_path);
+    _IMGFS_A_FILE_P root = already_file_list;
+    if(!format_path)
+        return 0;
+
+    _core_service_log_print("cat list about:");
+    _core_service_log_print(format_path);
+
+    if(len < 1){
+        free(format_path);
+        return 0;
+    }
+    if(len == 1){
+        if(format_path[0] == '/'){
+            free(format_path);
+            return makeUniTable(root);
+        }
+        else{
+            free(format_path);
+            return 0;
+        }
+    }
+    int lvl = fs_get_path_level(format_path);
+    char* name_t;
+    for(int i = 0; i <= lvl; i++){
+        name_t = fs_get_path_name(format_path, i);
+        root = (_IMGFS_A_FILE_P)already_file_get(root, name_t);
+        if(name_t)
+            free(name_t);
+    }
+    free(format_path);
+    return makeUniTable(root);
+}
+
+int imgfs::create(char *local_path) {
 
     return 0;
 }
